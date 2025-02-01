58463182e9647909bbf1d59f824a26a283a9a9ca
Fauzan <fauzan224321@gmail.com>
2025-02-01 11:44:16 +0700

    create code for connect to database

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn = mysqli_connect("DB_HOST", "DB_USER", "DB_PASSWROD", "DB_NAME");
+$conn = mysqli_connect("DB_HOST", "DB_USER", "DB_PASSWORD", "DB_NAME");
 
 if($conn->connect_error){
     die("Can't Connect To Database : " . $conn->connect_error);
