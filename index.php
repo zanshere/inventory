83711d0946ac7581544b0eb4e6b4c5446ad5f975
Fauzan <fauzan224321@gmail.com>
2025-02-03 11:04:54 +0700

    Menambahkan icon dan menambahkan sidebar

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
+<?php 
+include 'includes/header.php';
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home | inventory</title>
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
 <body>
