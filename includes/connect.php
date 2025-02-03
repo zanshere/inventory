84a286265f7a06de34833c95a03f1f0b64eebf18
fanfan-del <rwltv297@gmail.com>
2025-02-03 14:08:12 +0700

    gatau, memek

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn = mysqli_connect("localhost", "root", "Aditkontol123", "dashboard");
+$conn = mysqli_connect("localhost", "root", "", "dashboard");
 
 if($conn->connect_error){
     die("Can't Connect To Database : " . $conn->connect_error);
