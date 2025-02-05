4b11b066454964cea3819e988b9fc5ad29bf66ea
fanfan-del <rwltv297@gmail.com>
2025-02-05 08:07:59 +0700

    SEMUANYA

---
+++ pages/Admin/manage_users.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+include '../../includes/connect.php';
+include '../../functions/isUserBanned.php';
+
+$sidebarContent = '
+    <ul class="space-y-4">
+        <li>
+            <a href="#" @click.prevent="currentPage = \'customers\'; isOpen = false" class="text-gray-900 hover:text-gray-600 flex items-center">
+                <span class="material-icons mr-2">home</span>
+                Home
+            </a>
+        </li>
+        <li>
+            <a href="#" @click.prevent="currentPage = \'customers\'; isOpen = false" class="text-gray-900 hover:text-gray-600 flex items-center">
+                <span class="material-icons mr-2">manage_user</span>
+                manage_user
+
+            </a>
+        </li>
+        <li>
+            <a href="#" @click.prevent="currentPage = \'customers\'; isOpen = false" class="text-gray-900 hover:text-gray-600 flex items-center">
+                <span class="material-icons mr-2">people</span>
+                Controll Users
+            </a>
+        </li>
+        <li>
+            <a href="#" @click.prevent="currentPage = \'control_account\'; isOpen = false" class="text-gray-900 hover:text-gray-600 flex items-center">
+                <span class="material-icons mr-2">settings</span>
+                Controll Account
+            </a>
+        </li>
+    </ul>
+';
+
+include '../../includes/header.php';
+?>
