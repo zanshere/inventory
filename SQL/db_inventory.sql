68656fb890c89c2b95b9debd9077e2cac4714454
Fauzan <fauzan224321@gmail.com>
2025-02-19 17:52:58 +0700

    update database

--- SQL/db_inventory.sql
+++ SQL/db_inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 17 Feb 2025 pada 15.47
+-- Waktu pembuatan: 19 Feb 2025 pada 10.52
 -- Versi server: 9.2.0
 -- Versi PHP: 8.3.16
 
@@ -24,36 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `inventory`
---
-
-CREATE TABLE `inventory` (
-  `id` int NOT NULL,
-  `item_name` varchar(100) NOT NULL,
-  `description` text,
-  `quantity` int NOT NULL,
-  `added_by` int NOT NULL,
-  `added_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `logs`
---
-
-CREATE TABLE `logs` (
-  `id` int NOT NULL,
-  `user_id` int DEFAULT NULL,
-  `action` varchar(100) NOT NULL,
-  `action_details` text,
-  `action_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `users`
 --
 
@@ -65,36 +35,27 @@ CREATE TABLE `users` (
   `full_name` varchar(100) NOT NULL,
   `failed_attempts` int DEFAULT '0',
   `banned_until` datetime DEFAULT NULL,
+  `suspend_until` datetime DEFAULT NULL,
   `role` enum('admin','staff','user') DEFAULT 'user',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_active` tinyint(1) DEFAULT '1'
 ) ;
 
 --
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `full_name`, `failed_attempts`, `banned_until`, `role`, `created_at`) VALUES
-(1, 'admin', '$2a$12$8rsJGxWbISklitcWmDGOZOG9SR5UNJutI.1ErewxlU32vv2pU2RKq', 'admin@gmail.com', 'Admin', 0, NULL, 'admin', '2025-02-04 17:09:53'),
-(2, 'zanshere', '$2y$10$yTsICvg1Ay6RYfd7NCzbfOD6q/ANbPWTi60Ou9X00Bb.8A10wMiFq', 'fauzan224321@gmail.com', 'Muhammad Fauzan', 0, NULL, 'user', '2025-02-04 17:10:32'),
-(3, 'staff', '$2y$10$V491cNvH6yF/gVqYw/Fiwu/QrL43iX8GWUmnjSSz2dDabYzJRqs8C', 'staff@gmail.com', 'Staff', 0, NULL, 'staff', '2025-02-17 15:46:43');
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `full_name`, `failed_attempts`, `banned_until`, `suspend_until`, `role`, `created_at`, `is_active`) VALUES
+(1, 'admin', '$2a$12$8rsJGxWbISklitcWmDGOZOG9SR5UNJutI.1ErewxlU32vv2pU2RKq', 'admin@gmail.com', 'Admin', 0, NULL, NULL, 'admin', '2025-02-04 17:09:53', 1),
+(3, 'staff', '$2y$10$V491cNvH6yF/gVqYw/Fiwu/QrL43iX8GWUmnjSSz2dDabYzJRqs8C', 'staff@gmail.com', 'Staff', 0, NULL, '2025-02-19 10:19:39', 'staff', '2025-02-17 15:46:43', 1),
+(4, 'user', '$2y$10$g5yjpUIAwKci0sa587Vk4OlNtADdKy0BLPpI4d0PQPBAMEhBreC76', 'user@gmail.com', 'User', 0, NULL, NULL, 'user', '2025-02-19 10:27:55', 1),
+(5, 'zanshere', '$2y$10$q5Ro3VCIHcZBlLfSDDsBm.TBzzMfG1YnFNt3gnwRshXtMoeisshu6', 'fauzan224321@gmail.com', 'Muhammad Fauzan', 0, NULL, NULL, 'user', '2025-02-19 10:48:16', 1),
+(6, 'Mojangs', '$2y$10$J0ihp7KWf8ZWbpWS/I.EsuYYIOxWOmkuVULUPEzZ9zkMIJYvKRhmG', 'rektword@gmail.com', 'Rozan Fathin Yafi', 0, NULL, NULL, 'user', '2025-02-19 10:48:39', 1),
+(7, 'Kurogane', '$2y$10$rTIXLqgkmPyu4JzRMlX1MuML45qNLrwTC9fSU6ejFlQdqDh1bBbR6', 'itsryuzen1@gmail.com', 'Hikaru Kurogane', 0, NULL, NULL, 'user', '2025-02-19 10:48:57', 1);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indeks untuk tabel `inventory`
---
-ALTER TABLE `inventory`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `added_by` (`added_by`);
-
---
--- Indeks untuk tabel `logs`
---
-ALTER TABLE `logs`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indeks untuk tabel `users`
@@ -110,38 +71,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `inventory`
---
-ALTER TABLE `inventory`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `logs`
---
-ALTER TABLE `logs`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `inventory`
---
-ALTER TABLE `inventory`
-  ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`added_by`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `logs`
---
-ALTER TABLE `logs`
-  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
