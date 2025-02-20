d5d4b78936ef167f5ae6e49ccaaadb758c14534a
RaihanWoop <kentangcfc@gmail.com>
2025-02-20 16:08:46 +0700

    Update

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn = mysqli_connect("localhost", "root", "", "db_inventory");
+$conn = mysqli_connect("DB_NAME", "DB_USER", "DB_PASS", "YOUR_DB");
 
 if($conn->connect_error){
     die("Can't Connect To Database : " . $conn->connect_error);
