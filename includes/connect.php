f350e5f025b4bb04fea8ae3651a6915d847685d0
RaihanWoop <kentangcfc@gmail.com>
2025-02-20 15:46:01 +0700

    Membuat tampilan

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,12 @@
 <?php 
 
-$conn = mysqli_connect("DB_HOST", "DB_USER", "DB_PASS", "YOUR_DATABASE");
+$conn = mysqli_connect("localhost", "root", "", "db_inventory");
 
 if($conn->connect_error){
     die("Can't Connect To Database : " . $conn->connect_error);
 }
 
 ?>
+
+
+
--- pages/Admin/manage_inventory.php
+++ pages/Admin/manage_inventory.php
@@ -4,34 +4,44 @@ include '../../includes/connect.php';
 include '../../functions/isUserBanned.php';
 include '../../functions/errorControllers.php';
 
-$sidebarContent = '
-    <ul class="space-y-4">
-        <li>
-            <a href="#" @click.prevent="currentPage = \'customers\'; isOpen = false" class="text-gray-900 hover:text-gray-600 flex items-center">
-                <span class="material-icons mr-2">home</span>
-                Home
-            </a>
-        </li>
-        <li>
-            <a href="#" @click.prevent="currentPage = \'customers\'; isOpen = false" class="text-gray-900 hover:text-gray-600 flex items-center">
-                <span class="material-icons mr-2">manage_inventory</span>
-                Manage Inventory
-            </a>
-        </li>
-        <li>
-            <a href="#" @click.prevent="currentPage = \'customers\'; isOpen = false" class="text-gray-900 hover:text-gray-600 flex items-center">
-                <span class="material-icons mr-2">people</span>
-                Controll Users
-            </a>
-        </li>
-        <li>
-            <a href="#" @click.prevent="currentPage = \'control_account\'; isOpen = false" class="text-gray-900 hover:text-gray-600 flex items-center">
-                <span class="material-icons mr-2">settings</span>
-                Controll Account
-            </a>
-        </li>
-    </ul>
-';
 
-include '../../includes/header.php';
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include '../../includes/header.php'; ?>
+
+<div class="bg-white/10 backdrop-blur-lg rounded-xl p-6 shadow-lg">
+        <h1 class="text-2xl font-bold text-white mb-6 text-center">Manajemen Inventory</h1>
+        
+        <div class="overflow-x-auto">
+          <table class="w-full text-white border-collapse">
+            <thead class="bg-white/20">
+              <tr>
+                <th class="p-3 text-left rounded-tl-xl">No</th>
+                <th class="p-3 text-left">Nama Barang</th>
+                <th class="p-3 text-left">Jenis Barang</th>
+                <th class="p-3 text-left">Stok</th>
+                <th class="p-3 text-left">Tanggal Masuk</th>
+                <th class="p-3 text-left rounded-tr-xl">Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class= "p-3"></td>
+                    <td class= "p-3"></td>
+                    <td class= "p-3"></td>
+                    <td class= "p-3"></td>
+                    <td class= "p-3"></td>
+                    <td class= "p-3"></td>
+                </tr>
+            </tbody>
+</body>
+</html>
